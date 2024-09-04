756f66fcd3bc8ad28f3d47fed5e1207711a5a10f
kamiye <kamiye31@gmail.com>
2024-09-04 09:27:24 +0100

    navbar changes

--- dist/script.js
+++ dist/script.js
@@ -4,14 +4,7 @@ const closeMenuBtn = document.getElementById('close-menu-btn');
 
 menuBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('-translate-x-full');
-  mobileMenu.classList.add('translate-x-0');
-
-  // Trigger the fade-in animations
-  const menuItems = document.querySelectorAll('#mobile-menu button, #mobile-menu nav ul li a');
-  menuItems.forEach((item, index) => {
-    item.style.animationDelay = `${0.1 + index * 0.1}s`;
-    item.classList.add('opacity-100');
-  });
+  mobileMenu.classList.add('translate-x-0', 'open');
 });
 
 closeMenuBtn.addEventListener('click', () => {
@@ -26,12 +19,6 @@ document.addEventListener('click', (event) => {
 });
 
 function closeMenu() {
-  mobileMenu.classList.remove('translate-x-0');
+  mobileMenu.classList.remove('translate-x-0', 'open');
   mobileMenu.classList.add('-translate-x-full');
-
-  // Reset the opacity for the next open
-  const menuItems = document.querySelectorAll('#mobile-menu button, #mobile-menu nav ul li a');
-  menuItems.forEach(item => {
-    item.classList.remove('opacity-100');
-  });
 }
